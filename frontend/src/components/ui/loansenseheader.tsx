5604cdb7c0414eaf4a34f67ac156aa3d90edf711
Buhuni Levanya <buhunilevanya@gmail.com>
2026-02-20 12:21:40 +0530

    Resolve merge conflict in LoanSenseHeader

--- frontend/src/components/ui/loansenseheader.tsx
+++ frontend/src/components/ui/loansenseheader.tsx
@@ -1,32 +1,15 @@
 "use client";
-import { 
-  ArrowRight, 
-  ArrowUpRight, 
-  Bell, 
-  Calendar, 
-  Check, 
-  CheckCircle2, 
-  Info, 
-  Mail, 
-  RotateCcw, // For History
-  Search,
-  TrendingUp,
-  AlertCircle // For Partially Eligible
-} from "lucide-react";
 
-import React from 'react'
-
-
+import { Mail, Bell } from "lucide-react";
 
 type LoanSenseHeaderProps = {
   title: string;
-  subtitle?: string;
 };
 
-export default function LoansenseHeader( { title ='Dashboard', subtitle = 'User'} : LoanSenseHeaderProps) {
-  return ( 
-        <header className="flex flex-col md:flex-row justify-between items-center bg-[#0d3b66] text-white p-6 rounded-2xl shadow-lg gap-4">
-          <h1 className="text-2xl font-bold tracking-wide w-full md:w-auto">{title}</h1>
+export default function LoanSenseHeader({ title }: LoanSenseHeaderProps) {
+  return (
+    <header className="flex flex-col md:flex-row justify-between items-center bg-[#0d3b66] text-white p-6 rounded-2xl shadow-lg gap-4">
+          <h1 className="text-2xl font-bold tracking-wide w-full md:w-auto"> {title}</h1>
           <div className="flex items-center gap-6 w-full md:w-auto justify-end">
             <div className="flex gap-4">
                <button className="relative p-2 hover:bg-white/10 rounded-full transition-colors"><Mail size={20} /><span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full border border-[#0d3b66]"></span></button>
@@ -43,12 +26,8 @@ export default function LoansenseHeader( { title ='Dashboard', subtitle = 'User'
             </div>
           </div>
         </header>
-       
-  )
+  );
 }
-
-
-
 
 
 
